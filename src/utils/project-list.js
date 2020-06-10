4f04a786bbb7f63ea267794e6703970b75ceb2a6
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-10 15:18:20 -0500

    added Projects and work history

---
+++ src/utils/project-list.js
@@ -0,0 +1,76 @@
+let project_list = [
+	{
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/getFed.png',
+		title: 'GetFed Recipe App',
+		des:
+			"You get home and want to start a meal but don’t have an idea of what to make. This is where GetFed gets you started. Get recipes based on ingredients, items on hand, or restrictions. If you’re hungry, GetFed.",
+		tags: [ 'HTML', 'CSS', 'Bootstrap', 'JavaScript' ],
+		links: {
+			launch: 'https://dritchie3.github.io/GetFedProject',
+			github: 'https://github.com/Dritchie3/GetFed'
+		}
+	},
+	{
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/quarentainment.png',
+		title: 'Quarentainment',
+		des:
+			"Full Stack Project using ExpressJS and Passport (NodeJS) Development of a Social Media App. It allows for password authentication and hashing. Also, it allows entered password and responses to be saved in a MySQL database.",
+		tags: [ 'HTML', 'CSS', 'Javascript','Nodejs', 'Jquery', 'Bootstrap' ],
+		links: {
+			launch: 'https://quarintainment.herokuapp.com/',
+			github: 'https://github.com/Dritchie3/Quarintainment'
+		}
+	},
+	{  /* -------------------------------- */
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/WorkoutTracker.png',
+		title: 'Workout Tracker',
+		des:
+			"An application using MongoDB to store 'workout' data.  senting the email chip function adding and removing chips when sending emails",
+		tags: [ 'CSS', 'JavaScript', 'Nodejs', 'Bootstrap'],
+		links: {
+			launch: 'https://morning-everglades-96086.herokuapp.com/',
+			github: 'https://github.com/Dritchie3/workoutTracker'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/employeeDirectory.png',
+		title: 'Employee Directory',
+		des:
+			"Landing Page developed for Corefactors bangalore. Customer Relationship Management Service offered to help business manage their customers",
+		tags: [ 'HTML', 'CSS', 'React', 'JavaScript', 'JQuery' ],
+		links: {
+			launch: 'https://employee-directory-project-1.herokuapp.com/.',
+			github: 'https://github.com/Dritchie3/emplyeeDirectory'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/budgetTracker.png',
+		title: 'Budget Tracker',
+		des:
+			"A simple search application with ReactJS. Fetching Api from JSON placeholder to develop a set of robot cards where user can search names",
+		tags: [ 'React', 'HTML', 'CSS', 'JavaScript' ],
+		links: {
+			launch: 'https://budget-tracker-doug.herokuapp.com/',
+			github: 'https://github.com/Dritchie3/budgetTracker'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			'https://dritchie3.github.io/AboutMe/Images/weatherDash.png',
+		title: 'Weather Dashboard',
+		des:
+			"Javascript Clock is an application built with VanillaJS , where local time is fetched and shown in a clock. It has hour, minute and second hands",
+		tags: ['HTML', 'CSS', 'JavaScript' ],
+		links: {
+			launch: 'https://dritchie3.github.io/weatherDashboard/',
+			github: 'https://github.com/Dritchie3/weatherDashboard'
+		}
+	}
+];
+
+export default project_list;
