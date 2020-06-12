0cff7cfa902941b0ed8f24b456f063f2e54d5136
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-12 16:56:32 -0500

    add .env

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+// const express = require('express');
+// const favicon = require('express-favicon');
+// // const morgan = require('morgan');
+// const path = require('path');
+// const PORT = process.env.PORT || 3000;
+// const app = express();
+
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("build"));
+// }
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`App running on https://localhost/${PORT} !`)
+// });
+
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-}
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -17,6 +40,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(PORT, () => {
-    console.log(`App running on https://localhost/${PORT} !`)
-});
+app.listen(port);
