94e28f6f0b43f58fb598adae5bc67464cc7d8c92
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-15 16:16:37 -0500

    add build

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
-const express = require('express');
+const express = require("express");
 const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 3000;
+
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(favicon(__dirname + 'client/src/assets/self_photo.jpg'));
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+
+
+
+
+
+// const express = require('express');
+// const favicon = require('express-favicon');
+// const path = require('path');
+// const port = process.env.PORT || 3001;
+// const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+// app.listen(port);
+
+
+
 
 // const express = require('express');
 // const favicon = require('express-favicon');
@@ -92,30 +126,3 @@ app.listen(port);
 
 
 
-
-// const express = require("express");
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("/build"));
-// }
-// // Add routes, both API and view
-
-// // change the path to your index.html
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname+'/public/index.html'); 
-// });
-
-// // Connect to the Mongo DB
-
-
-// // Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on http://localhost:${PORT}`);
-// });
