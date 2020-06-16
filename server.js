c0bc5282b71a0e012c1cb697b0f95840fa45707b
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-15 22:07:40 -0500

    add readMe URLs

--- server.js
+++ server.js
@@ -26,68 +26,8 @@ app.listen(PORT, function() {
 
 
 
-
-
-
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const path = require('path');
-// const port = process.env.PORT || 3001;
-// const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-// app.listen(port);
-
-
-
-
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const logger = require("morgan");
-// const path = require('path');
-// const PORT = process.env.PORT || 3000;
-// const app = express();
-
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// app.use(logger("dev"));
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("build"));
-// }
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname+'../src/utils/index.js'); // change the path to your index.html
-// });
-
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, '../public/index.js')));
-
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-// // app.get('/*', function (req, res) {
-// //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// // });
-
-// app.listen(PORT, () => {
-//     console.log(`App running on https://localhost/${PORT} !`)
-// });
-
-
-
-
-
-
-
 // this code will allow for email to be sent to me.
+
 // const nodemailer = require('nodemailer');
 // require("dotenv").config();
 
