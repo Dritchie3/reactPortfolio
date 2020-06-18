7590df581963779d7dd84e18a97ee439ae88fa8b
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-18 14:50:12 -0500

    rewrite file structure to not allow production build

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 const express = require("express");
 const favicon = require('express-favicon');
-
+const path = require('path');
+const PORT = process.env.PORT || 3002;
 const app = express();
 
-const PORT = process.env.PORT || 3002;
 
 
-app.use(favicon(__dirname + 'client/src/assets/self_photo.jpg'));
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.post('/contact', function (req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'yahoo',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+  
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+});
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // console.log(`--------------before ${production} ----------------`);
 // app.get('/*', function (req, res) {
@@ -19,9 +56,9 @@ app.use(express.json());
 // console.log(`--------------after ${production} ----------------`);
 // app.use('/', express.static(path.join(__dirname, 'build/index.html')))
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build/index.html"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build/index.html"));
+// }
 // Add routes, both API and view
 // app.use(routes);
 
