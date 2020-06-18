b25c1c01023b8c8711eb9bc187e67538f9931bb7
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-18 13:51:20 -0500

    change prod path on server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(express.json());
 //   res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
 // });
 // console.log(`--------------after ${production} ----------------`);
-
+// app.use('/', express.static(path.join(__dirname, 'build/index.html')))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/static"));
+  app.use(express.static("build/index.html"));
 }
 // Add routes, both API and view
 // app.use(routes);
