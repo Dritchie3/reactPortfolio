18ff1136f9daf4f7e2d90fa44793ea10d457222f
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-18 13:32:37 -0500

    Remove Production start from package.json

--- server.js
+++ server.js
@@ -1,34 +1,60 @@
 const express = require("express");
-const favicon = require('express-favicon');
 
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
 const app = express();
-
-const PORT = process.env.PORT || 3002;
-
-
-app.use(favicon(__dirname + 'client/src/assets/self_photo.jpg'));
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// console.log(`--------------before ${production} ----------------`);
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
-});
-// console.log(`--------------after ${production} ----------------`);
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/static"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 // app.use(routes);
+
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+
+// const express = require("express");
+// const favicon = require('express-favicon');
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3002;
+
+
+// app.use(favicon(__dirname + 'client/src/assets/self_photo.jpg'));
+
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // console.log(`--------------before ${production} ----------------`);
+// // app.get('/*', function (req, res) {
+// //   res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+// // });
+// // console.log(`--------------after ${production} ----------------`);
+
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build/static"));
+// }
+// // Add routes, both API and view
+// // app.use(routes);
+
+// // Start the API server
+// app.listen(PORT, function() {
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
 
 
 
