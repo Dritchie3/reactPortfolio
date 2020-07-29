30cdcb17ea6588fecce51e55fcfd21887b78e823
Doug Ritchie <dritchie3@sbcglobal.net>
2020-07-29 17:10:00 -0500

    update experience story

--- src/utils/project-list.js
+++ src/utils/project-list.js
@@ -1,14 +1,14 @@
 let project_list = [
 	{
 		image:
-			'https://github.com/Dritchie3/reactPortfolio/src/assets/projects/CPC.png ',
+			'https://dritchie3.github.io/AboutMe/Images/CPC.png',
 		title: 'CodeProConnection App',
 		des:
 			"This is a full-Stack app using MongoDB, Mongoose, Jquery, Bootstrap, HTML, Passport for authentication, and JSON for api calls.  It allows Freelance workers to connect to project hiring managers.  The idea is to allow the managers, (it does not have to be a manager, it could be anyone that needs work done), to post jobs for websites, apps, and electronic media that thay need freelancers for.  It allows the hirer to post the jobs and the freelancer to apply for the job.  Currently the sorting feature and the contact/mail-to features are in progress. ",
 		tags: [ 'React', 'NodeJS', 'MongoDB', 'Mongoose', 'CSS', 'Bootstrap', 'Jquery' ],
 		links: {
 			launch: 'https://cpc-project3.herokuapp.com/',
-			github: 'https://github.com/Dritchie3/CPC'
+			github: 'https://github.com/shamlin143/Codepro2'
 		}
 	},
 	{
